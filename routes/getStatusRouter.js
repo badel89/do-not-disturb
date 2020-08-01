72c683b198957e7a95be806f0aec3cd6b91c92b1
badel89 <mbadel98@gmail.com>
2020-08-01 13:37:24 -0500

    added color as argument passed

--- routes/getStatusRouter.js
+++ routes/getStatusRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
     let status = req.app.get("status");
+    let color = req.app.get("color");
     res.statusCode = 200;
-    res.send(status);
+    res.send(JSON.stringify({ "status": status, "color": color }));
 });
 
 module.exports = router;
--- routes/setStatusRouter.js
+++ routes/setStatusRouter.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.post("/", function(req, res, next) {
     // set status.
-    req.app.set('status', req.body.status);
+    req.app.set("status", req.body.status);
+    req.app.set("color", req.body.color);
     res.statusCode = 200;
     res.send("OK");
 });
